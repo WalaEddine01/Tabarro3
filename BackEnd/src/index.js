c6a3b4713d9889a5d64adc33002f480848bebfe2
walaeddine01 <walaaeddine33@gmail.com>
2024-02-17 15:21:20 +0100

    I made the changes on 2024-02-17 15:21:20

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,17 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+const { initializeApp } = require('firebase/app');
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB7dTxA5LTDSKAFTlyJ3l_YYeMikxqIWGc",
-  authDomain: "tabaro3-9ce2a.firebaseapp.com",
-  projectId: "tabaro3-9ce2a",
-  storageBucket: "tabaro3-9ce2a.appspot.com",
-  messagingSenderId: "1019511077492",
-  appId: "1:1019511077492:web:a88b77c611834300c9ef21"
+  apiKey: 'AIzaSyB7dTxA5LTDSKAFTlyJ3l_YYeMikxqIWGc',
+  authDomain: 'tabaro3-9ce2a.firebaseapp.com',
+  projectId: 'tabaro3-9ce2a',
+  storageBucket: 'tabaro3-9ce2a.appspot.com',
+  messagingSenderId: '1019511077492',
+  appId: '1:1019511077492:web:a88b77c611834300c9ef21'
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
